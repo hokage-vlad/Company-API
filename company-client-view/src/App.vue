a991a39a7cd10cecb4e9c70028f6d8c3a4421105
hyper-vlad <hypervlados@gmail.com>
2020-09-30 16:07:00 +0300

    Navbar start +

--- company-client-view/src/App.vue
+++ company-client-view/src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <Books msg="Books App"/>
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        <Navbar></Navbar>
+        <Books msg="Books App"/>
+        <router-view></router-view>
+    </div>
 </template>
 
 <script>
-import Books from './components/Books.vue'
+    import Navbar from './components/NavBar'
+    import Books from './components/Books.vue'
 
-export default {
-  name: 'App',
-  components: {
-    Books
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Navbar,
+            Books
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
 </style>
