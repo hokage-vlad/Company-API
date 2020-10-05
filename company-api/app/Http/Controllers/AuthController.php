f723d31cb01472d2f70db2b581f635a79babb0dc
hyper-vlados <hypervlados@gmail.com>
2020-10-05 17:53:17 +0300

    Found hash check

--- company-api/app/Http/Controllers/AuthController.php
+++ company-api/app/Http/Controllers/AuthController.php
@@ -17,7 +17,7 @@ class AuthController extends Controller
     {
 
         $user = User::where('email', $request->email)->first();
-        
+
         if (!$user || !Hash::check($request->password, $user->password)) {
             throw ValidationException::withMessages([
                 'email' => ['Incorrect']
